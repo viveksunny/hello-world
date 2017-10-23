e51e9347682515bbd21522a8ec9fabfbe211a553
Vivek Bharadwaj <sunny.vivek28122009@gmail.com>
2017-10-23 22:57:06 +0530

    Change the variable names to sensible variale names

--- Assignmen.py
+++ Assignmen.py
@@ -5,29 +5,29 @@ cursor = connection.cursor()
 querystring = "select * from Employees"
 cursor.execute(querystring)
 fo = open("Employee.txt",'a+')
-for i in cursor:
-    for j in i:
-        if j == 7:
-            fo.write(float(j))
+for row_elements in cursor:
+    for column_element in row_elements:
+        if column_element == 7:
+            fo.write(float(column_element))
             fo.write(',')
         else:
-            fo.write(str(j)+',')
+            fo.write(str(column_element)+',')
     fo.write("\n")
 fo.close()
 #to read the file line by line and write in another file
 fo = open("Employee.txt",'r')
 nfo = open("Updated.txt",'w+')#to save the updated text
-for line in fo.readlines():
-    line=line.split(",")
-    if line[6] == "IT_PROG":
-        line[7]=float(line[7])*1.1
-        line[7]=str(line[7])
-        qstring = "update Employees set salary = "+ line[7] +" where EMPLOYEE_ID = " + line[0]
-        cursor.execute(qstring)
-        nfo.write(str(line))
+for each_row_line in fo.readlines():
+    each_row_line = each_row_line.split(",")
+    if each_row_line[6] == "IT_PROG":
+        each_row_line[7]=float(each_row_line[7])*1.1
+        each_row_line[7]=str(each_row_line[7])
+        Update_string = "update Employees set salary = "+ each_row_line[7] +" where EMPLOYEE_ID = " + each_row_line[0]
+        cursor.execute(Update_string)
+        nfo.write(str(each_row_line))
         nfo.write('\n')
-qstring="commit"
-cursor.execute(qstring)
+Commit_string="commit"
+cursor.execute(Commit_string)
 print("Update Successful")
 fo.close()
 nfo.close()
